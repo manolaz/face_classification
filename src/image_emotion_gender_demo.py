94baf522167a045303d810ed8dbc1e92e4e3c704
Anh Trung Nguyen Vu <trungupdate@gmail.com>
2018-11-16 14:28:29 +0700

    modified live emotion detection demo

--- src/image_emotion_gender_demo.py
+++ src/image_emotion_gender_demo.py
@@ -16,7 +16,7 @@ from utils.preprocessor import preprocess_input
 # parameters for loading data and images
 image_path = sys.argv[1]
 detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
-emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
+# emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
 gender_model_path = '../trained_models/gender_models/simple_CNN.81-0.96.hdf5'
 emotion_labels = get_labels('fer2013')
 gender_labels = get_labels('imdb')
@@ -30,11 +30,11 @@ emotion_offsets = (0, 0)
 
 # loading models
 face_detection = load_detection_model(detection_model_path)
-emotion_classifier = load_model(emotion_model_path, compile=False)
+# emotion_classifier = load_model(emotion_model_path, compile=False)
 gender_classifier = load_model(gender_model_path, compile=False)
 
 # getting input model shapes for inference
-emotion_target_size = emotion_classifier.input_shape[1:3]
+# emotion_target_size = emotion_classifier.input_shape[1:3]
 gender_target_size = gender_classifier.input_shape[1:3]
 
 # loading images
@@ -49,11 +49,11 @@ for face_coordinates in faces:
     rgb_face = rgb_image[y1:y2, x1:x2]
 
     x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
-    gray_face = gray_image[y1:y2, x1:x2]
+    # gray_face = gray_image[y1:y2, x1:x2]
 
     try:
         rgb_face = cv2.resize(rgb_face, (gender_target_size))
-        gray_face = cv2.resize(gray_face, (emotion_target_size))
+        # gray_face = cv2.resize(gray_face, (emotion_target_size))
     except:
         continue
 
@@ -63,11 +63,11 @@ for face_coordinates in faces:
     gender_label_arg = np.argmax(gender_prediction)
     gender_text = gender_labels[gender_label_arg]
 
-    gray_face = preprocess_input(gray_face, True)
-    gray_face = np.expand_dims(gray_face, 0)
-    gray_face = np.expand_dims(gray_face, -1)
-    emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
-    emotion_text = emotion_labels[emotion_label_arg]
+    # gray_face = preprocess_input(gray_face, False)
+    # gray_face = np.expand_dims(gray_face, 0)
+    # gray_face = np.expand_dims(gray_face, -1)
+    # emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+    # emotion_text = emotion_labels[emotion_label_arg]
 
     if gender_text == gender_labels[0]:
         color = (0, 0, 255)
@@ -76,7 +76,10 @@ for face_coordinates in faces:
 
     draw_bounding_box(face_coordinates, rgb_image, color)
     draw_text(face_coordinates, rgb_image, gender_text, color, 0, -20, 1, 2)
-    draw_text(face_coordinates, rgb_image, emotion_text, color, 0, -50, 1, 2)
+    # draw_text(face_coordinates, rgb_image, emotion_text, color, 0, -50, 1, 2)
 
 bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
-cv2.imwrite('../images/predicted_test_image.png', bgr_image)
+# cv2.imwrite('../images/predicted_test_image.png', bgr_image)
+result_filename = input('Enter export result file name: ')
+cv2.imwrite('../result_images/predicted_test_image_' +
+            result_filename + '.png', bgr_image)
